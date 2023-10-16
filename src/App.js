b6446e75dd9f3f7eb1bf63515aa489b668d09863
VisHaL0023 <vishalpanchal1828vp@gmail.com>
2023-10-16 23:19:17 +0530

    added hero section and some dummy card from API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import Hero from "./components/HeroSection/Hero";
 import NavBar from "./components/NavBar/NavBar";
+import { fetchTopAlbums } from "./API/api";
+import { useEffect, useState } from "react";
+import Card from "./components/Card/Card";
 
 function App() {
+  
+  const [topAlbums, setTopAlbums] = useState([]);
+
+  const generateData = async () =>{
+    const data = await fetchTopAlbums();
+    setTopAlbums(data);
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    generateData();
+  }, []);
+
   return (
     <div className="App">
       < NavBar />
+
+      <Hero />
+
+      {topAlbums.map((item) => {
+        return (
+          <Card key ={item.id} data = {item} type="album" />
+        )
+      })}
     </div>
   );
 }
---
+++ src/components/HeroSection/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+import Headphone from "../../assets/headphone.svg";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+        <div>
+            <h1>100 Thousand Songs, ad fee</h1>
+            <h1>Over thhousands podcast episodes</h1>
+            <div>
+                <img src ={Headphone} width={212} alt="headphones" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
